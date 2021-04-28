0d1b1c45cd04fd73e4639e2a296875dde08fe5b8
Eldian Restorationist <eldianrestorationist@gmail.com>
2021-04-27 19:35:45 -0500

    added psudocode for basis

---
+++ etch-a-sketch.js
@@ -0,0 +1,12 @@
+//variable for columns of divs
+//columns = rows
+//set to 16 to start
+//function to make grid of square divs
+//set background color of divs to white
+//eventlistener for mouse enter //function change color divs
+//button to change color option
+//function for random color 
+//button to reset grid
+//button to change grid size
+//alert prompt to enter new grid size from user input
+//input max 100
